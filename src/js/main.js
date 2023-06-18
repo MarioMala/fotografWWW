4c350d7aecf7d33eb8eaf95da35cf27738743d09
Mario <malasiewicz.mariusz@gmail.com>
2023-06-18 13:30:26 +0200

    Dodadana wersja mobil (telefony)

--- src/js/main.js
+++ src/js/main.js
@@ -1,51 +1,58 @@
 /** @format */
 
-const footerYear = document.querySelector('.footer__year')
-const btn = document.querySelector('.return')
-const cookieBox = document.querySelector('.cookie__box')
-const cookieBtn = document.querySelector('#btn')
-let root = document.documentElement
+const footerYear = document.querySelector('.footer__year');
+const btn = document.querySelector('.return');
+const cookieBox = document.querySelector('.cookie__box');
+const cookieBtn = document.querySelector('#btn');
+const mobileBtn = document.querySelector('#burger');
+const mobileMenu = document.querySelector('.container .nav__box-mobile');
+let root = document.documentElement;
 
 // ----------------------------------Rok w footer---------------------------------------------------------
 
 const handleCurrentYear = () => {
-	const year = new Date().getFullYear()
-	footerYear.innerText = year
-}
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
 
-handleCurrentYear()
+handleCurrentYear();
 
 const handleScroll = () => {
-	const scroll = window.scrollY
-	const leftToScroll = document.body.getBoundingClientRect().height - window.innerHeight
+	const scroll = window.scrollY;
+	const leftToScroll = document.body.getBoundingClientRect().height - window.innerHeight;
 
-	const scrollBarWidth = Math.floor((scroll / leftToScroll) * 100)
+	const scrollBarWidth = Math.floor((scroll / leftToScroll) * 100);
 
 	if (scrollBarWidth > 10) {
-		btn.classList.add('active')
-	} else btn.classList.remove('active')
-}
+		btn.classList.add('active');
+	} else btn.classList.remove('active');
+};
 
 const handleScrollTop = () => {
 	window.scroll({
 		top: 0,
 		behavior: 'smooth',
-	})
-}
+	});
+};
 
 const showCookie = () => {
-	const cookieEaten = localStorage.getItem('cookie')
+	const cookieEaten = localStorage.getItem('cookie');
 	if (cookieEaten) {
-		cookieBox.classList.add('hide')
+		cookieBox.classList.add('hide');
 	}
-}
+};
 
 const handleCookieBox = () => {
-	localStorage.setItem('cookie', 'true')
-	cookieBox.classList.add('hide')
-}
+	localStorage.setItem('cookie', 'true');
+	cookieBox.classList.add('hide');
+};
 
-window.addEventListener('scroll', handleScroll)
-btn.addEventListener('click', handleScrollTop)
-cookieBtn.addEventListener('click', handleCookieBox)
-showCookie()
+const showMobileMenu = () => {
+	mobileMenu.classList.toggle('active');
+};
+
+window.addEventListener('scroll', handleScroll);
+btn.addEventListener('click', handleScrollTop);
+cookieBtn.addEventListener('click', handleCookieBox);
+mobileBtn.addEventListener('click', showMobileMenu);
+showCookie();
